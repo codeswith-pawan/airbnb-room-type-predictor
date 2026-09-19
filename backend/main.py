f9d8bfeadfc6720570eb434d1139d43c5701171e
Pawan Sharma <pawanbtechcse108@gmail.com>
2026-09-19 23:39:34 +0530

    Allow Vercel frontend in CORS

--- backend/main.py
+++ backend/main.py
@@ -11,11 +11,13 @@ app = FastAPI(
     version="1.0"
 )
 
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://127.0.0.1:5173"
+        "http://127.0.0.1:5173",
+        "https://airbnb-room-type-predictor-phi.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
